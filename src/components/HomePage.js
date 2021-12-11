e7aa9934cabd9490fdb39435a8b803898edc27ed
abirm2 <Mansouriabir96@gmail.com>
2021-12-11 18:54:19 +0100

    router

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className="home">
+            <h1>Welcome To ShofTN</h1>
+            
+        </div>
+    )
+}
+
+export default HomePage
